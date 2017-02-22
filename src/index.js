b68023785321202128978cf63defca95ebe72ea5
marcohamersma <hello@marcohamersma.nl>
2017-02-22 18:10:48 +0000

    Correct paths for demo website router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, browserHistory, Redirect } from 'react-router';
 import { RouterLayout as StyleGuide } from 'react-easy-styleguide';
 import './index.css';
 
@@ -10,14 +10,14 @@ const styleguideContext = require.context('./components/', true, /.info.js$/)
 
 ReactDOM.render(
   <Router history={browserHistory}>
-    <Route path="/" component={App}/>
     <Route
-      path="/styleguide(/:component(/:variation))"
+      path="/react-easy-styleguide/(/:component(/:variation))"
       component={StyleGuide(styleguideContext, {
         name: 'My StyleGuide',
         propTypes: React.PropTypes
       })}
     />
+    <Redirect from="/" to="/react-easy-styleguide/" />
   </Router>,
   document.getElementById('root')
 );
